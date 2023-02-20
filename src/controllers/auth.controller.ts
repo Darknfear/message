28fb34cf4687148f8df946a54fc4620403c02a93
hieu <hieuact13011998@gmail.com>
2023-02-20 16:36:31 +0700

    Update config

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 import { InternalServerError } from 'http-errors';
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-  throw InternalServerError()
-  console.log(next);
   return res.json({ data: true });
 }
